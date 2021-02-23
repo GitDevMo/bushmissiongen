4715b28020d0de6f786a93e5ebad5599231023bf
f99mlu <mats.lundquist75@gmail.com>
2021-02-23 21:16:48 +0100

    Prepare for next version

--- src/main/java/bushmissiongen/BushMissionGen.java
+++ src/main/java/bushmissiongen/BushMissionGen.java
@@ -52,7 +52,7 @@ import bushmissiongen.misc.SimData;
  * @author  f99mlu
  */
 public class BushMissionGen {
-	public static final String VERSION = "1.69";
+	public static final String VERSION = "1.70";
 
 	// NEWS
 	// - 
